f6ad0a7c9f02c46b3c0e1863c24a6c51d8ab5530
AllanDin5 <91555203+AllanDin5@users.noreply.github.com>
2022-09-01 18:41:02 +0200

    Appbar mal gestyled
    Appbar mit on click action hinzugefügt
    Wird noch mit Variablen angepasst da sich das ändert

--- mimir/src/AppBar.tsx
+++ mimir/src/AppBar.tsx
@@ -1,10 +1,36 @@
 import styled from 'styled-components/macro'
 
 export function AppBar() {
-    return <Bar>My App</Bar>
+    return <Bar>
+        <TitleText>Mirmir</TitleText>
+        <TopButton onClick={sayHello}>New Game </TopButton>
+        <ManageCardsSetupButton onClick={sayHello}>Manage Cards</ManageCardsSetupButton>
+
+    </Bar>
 }
 
 const Bar = styled.div`
-  background: lightblue;
-  padding: 10px;
+  background: darkblue;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
 `
+const TitleText = styled.div`
+  color: white;
+`
+const ManageCardsSetupButton = styled.button`
+  color: lightblue;
+  background: darkblue;
+  border-width: 0px;
+`
+const TopButton = styled.button`
+  background-color: lightblue;
+  color: white;
+  border-radius: 5px;
+  border-width: 0px;
+  
+`
+function sayHello() {
+    alert('You clicked me!');
+}
