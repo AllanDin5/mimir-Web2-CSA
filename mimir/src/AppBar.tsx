7174ecebce368d623dc2e5a60da364130ff04f65
AllanDin5 <91555203+AllanDin5@users.noreply.github.com>
2022-09-01 19:08:38 +0200

    Startpage & Appbar
    StartPage:Added Button and css on index home
    Appbar:
    Added Aligment

--- mimir/src/AppBar.tsx
+++ mimir/src/AppBar.tsx
@@ -5,7 +5,6 @@ export function AppBar() {
         <TitleText>Mirmir</TitleText>
         <TopButton onClick={sayHello}>New Game </TopButton>
         <ManageCardsSetupButton onClick={sayHello}>Manage Cards</ManageCardsSetupButton>
-
     </Bar>
 }
 
@@ -13,23 +12,24 @@ const Bar = styled.div`
   background: darkblue;
   padding: 15px;
   display: flex;
-  justify-content: space-between;
   align-items: baseline;
 `
 const TitleText = styled.div`
   color: white;
-`
-const ManageCardsSetupButton = styled.button`
-  color: lightblue;
-  background: darkblue;
-  border-width: 0px;
+  align-self: flex-start;
 `
 const TopButton = styled.button`
   background-color: lightblue;
   color: white;
   border-radius: 5px;
   border-width: 0px;
-  
+  align-self: center;
+`
+const ManageCardsSetupButton = styled.button`
+  color: lightblue;
+  background: darkblue;
+  border-width: 0px;
+  align-self: flex-end;
 `
 function sayHello() {
     alert('You clicked me!');
