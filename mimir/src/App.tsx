8fb786764a58c566082359524f4f865c022a387b
AllanDin5 <91555203+AllanDin5@users.noreply.github.com>
2022-09-01 17:57:18 +0200

    Added style componendds and Routes
    npm install -S react-router-dom
    npm i styled-components @types/styled-components
    Took first example from page
    https://codesandbox.io/s/elastic--ritchieritchie--phmzv8?file=/src/App.tsx:0-291

--- mimir/src/App.tsx
+++ mimir/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { Layout } from './Layout'
+import { Home } from './Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    )
 }
-
-export default App;
--- mimir/src/index.tsx
+++ mimir/src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    height: 100%;
+    margin: 0;
+    font-family: sans-serif;
+  }
+`
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <StrictMode>
+        <GlobalStyle />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+)
